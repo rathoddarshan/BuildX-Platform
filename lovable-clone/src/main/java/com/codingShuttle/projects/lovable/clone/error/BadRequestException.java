42a5b56d09c6e646b2958f8f4220f766e8374ab7
rathoddarshan <rathoddarshan075@gmail.com>
2026-02-12 00:03:30 +0530

    Add small exception handler error

--- lovable-clone/src/main/java/com/codingShuttle/projects/lovable/clone/error/BadRequestException.java
+++ lovable-clone/src/main/java/com/codingShuttle/projects/lovable/clone/error/BadRequestException.java
@@ -1,9 +1,10 @@
 package com.codingShuttle.projects.lovable.clone.error;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
+@Getter
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class BadRequestException extends RuntimeException{
