c28f81a8532360c0f0393808c77221abbeaa2483
rathoddarshan <rathoddarshan075@gmail.com>
2026-03-10 23:30:28 +0530

    Add Authorization

--- BuildX-PlatForm/src/main/java/com/codingShuttle/projects/buildX/platform/repository/ProjectRepository.java
+++ BuildX-PlatForm/src/main/java/com/codingShuttle/projects/buildX/platform/repository/ProjectRepository.java
@@ -16,9 +16,9 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
             SELECT p FROM Project p
             WHERE p.deletedAt IS NULL
             AND EXISTS (
-                SELECT 1 FROM projectMemember pm
+                SELECT 1 FROM ProjectMember pm
                 WHERE pm.id.userId = :userId
-                AND pm.id.projectId = :p.id
+                AND pm.id.projectId = p.id
             )
             ORDER BY p.updatedAt DESC
             """
@@ -30,6 +30,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
         SELECT p FROM Project p
         WHERE p.id = :projectId
             AND p.deletedAt IS NULL
+            AND EXISTS (
+            SELECT 1 FROM ProjectMember pm
+            WHERE pm.id.userId = :userId
+            AND pm.id.projectId = p.id
+            )
         """)
     Optional<Project> findAccessibleProjectById(
             @Param("projectId") Long projectId,
